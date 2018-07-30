536bc47502768a96316ac5ea34b2b86a3b0d9710
duhao <dh@zjtachao.com>
2018-07-30 10:19:54 +0800

    初始化项目

--- test.py
+++ test.py
@@ -5,7 +5,7 @@
  * Copyright (c) 2015-2017 Zhejiang TaChao Network Technology Co.,Ltd.
  * All rights reserved.
  *
- * 项目名称：浙江踏潮-新概念英语
+ * 项目名称：浙江踏潮
  * 版权说明：本软件属浙江踏潮网络科技有限公司所有，在未获得浙江踏潮网络科技有限公司正式授权
  *           情况下，任何企业和个人，不能获取、阅读、安装、传播本软件涉及的任何受知
  *           识产权保护的内容。
@@ -13,7 +13,7 @@
 '''
 
 '''
- * TODO
+ * 测试Flask
  * @author <a href="mailto:dh@zjtachao.com">duhao</a>
 '''
 import datetime ,time ,json
@@ -23,6 +23,7 @@ from flask_redis import FlaskRedis
 from redis import StrictRedis
 from flask_sqlalchemy import SQLAlchemy
 
+#redis
 REDIS_URL = "redis://:zjtachao@192.168.1.12:6379/0"
 
 app = Flask(__name__)
@@ -30,6 +31,7 @@ app.config['REDIS_URL'] = REDIS_URL
 api = Api(app)
 redis_store = FlaskRedis.from_custom_provider(StrictRedis , app)
 
+#mysql
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://zjtachao:123456@192.168.1.11:3306/ww?charset=utf8"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
